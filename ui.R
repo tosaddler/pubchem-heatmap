cb9bfea55fd6651609456958a9fbccf01eb579af
Trey Saddler <treyosaddler@gmail.com>
2017-08-03 16:19:39 -0400

    Adds one more initial compound

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      textAreaInput("chemid", "Input PubChem CIDs", value = "6618\n1983"),
+      textAreaInput("chemid", "Input PubChem CIDs", value = "6618\n1983\n120228"),
       checkboxGroupInput("categories", "Select categories", c("Pharmacology and Biochemistry", "Use and Manufacturing", "Identification", "Safety and Hazards", "Toxicity"),
                          selected = c("Pharmacology and Biochemistry", "Use and Manufacturing", "Identification", "Safety and Hazards", "Toxicity"))
     ),
